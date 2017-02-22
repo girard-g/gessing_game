effbcd2d3765eeb5136589891686124705d47e79
guillaume <guillaumegirardpro@gmail.com>
2017-02-22 20:00:23 +0100

    rand generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number !");
+
+    let secret_member = rand::thread_rng().gen_range(1, 101);
+
+    println!("Please input your guess.");
+
+    println!("The secret number is {}", secret_member);
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+ 
+    println!("You guessed: {}", guess);
 }
