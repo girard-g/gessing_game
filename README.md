31db1bcd1086177a820447d25730a760ba9c6df2
Evergreenn <evergreen@protonmail.ch>
2017-02-23 10:47:17 +0100

    final commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Guessing game
+
+This is a small guessing game in rust
+
+## Usage
+
+You need to run main typing
+
+```
+$ ./main
+```
+
+## Installation
+
+If you want to write some rust code you need to install rust on your side
+
+```
+$ curl https://sh.rustup.rs -sSf | sh
+``` 
+
+This will install rust and cargo which is a dependencies manager like composer
+
+Open up a shell and type
+
+```
+rustc --version
+```
+and
+```
+cargo --version
+```
+
+You should see the version number
+
+For more information go to [the offical rust doc](https://doc.rust-lang.org/book/getting-started.html)
+
+## Tips
+
+I wrote this with vim, there is a plugin for rust [here](https://github.com/rust-lang/rust.vim)
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess the number !");
 
-    let secret_member = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("Please input your guess.");
 
-    println!("The secret number is {}", secret_member);
+    loop {
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io:: stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_)  => continue, 
+        };
  
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too big!"),
+            Ordering::Equal     => {
+                println!("You win");
+                break;
+            }
+        }
+
+    }
 }
